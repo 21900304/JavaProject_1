0345887413313a25eac2425a6c18d8abdcd5e8d9
21900304 <21900304@handong.ac.kr>
2022-09-02 20:46:01 +0900

    first commit

---
+++ src/main/java/com/myword/word/Main.java
@@ -0,0 +1,11 @@
+package com.myword.word;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new WordManager().start();
+        // TODO Auto-generated method stub
+
+    }
+
+}
